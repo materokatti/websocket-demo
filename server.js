a00c0575a060a1a233cf4114ff2ecda1ba901258
Doyun <materokatti@gmail.com>
2025-01-02 15:46:40 +0900

    add: specify url

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Server running on port *:${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
